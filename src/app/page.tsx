390abcac4285e392887ab6966e7b58ad585fc3a0
Jose Rogerio de Almeida Junior <jose.junior@andritz.com>
2023-08-26 15:01:07 -0300

    feat(home): add photo gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,45 @@
+import { cn } from '@/lib/utils'
+import * as Icons from 'lucide-react'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <>
+    <div className="flex w-full flex-col">
       <Header />
       <section>
         <Indication posX="left" posY="upper" />
-        <Indication posX="left" posY="upper" />
+        <Indication posX="right" posY="upper" />
         <Indication posX="right" posY="lower" />
         <Indication posX="right" posY="lower" />
       </section>
-    </>
+      <section className="flex flex-col gap-10 px-40">
+        <header>
+          <h2 className="flex gap-2 text-2xl">
+            <span className="grow">Conheça o meu trabalho...</span>
+            <Icons.Instagram size="2rem" />
+            <span>@mandytattoo</span>
+          </h2>
+        </header>
+        <div className="grid grid-cols-3 gap-4">
+          {/* TODO: adjust alts */}
+          <Image src="/work-001.jpg" alt="..." width={2861} height={3603} />
+          <Image src="/work-001.jpg" alt="..." width={2861} height={3603} />
+          <Image src="/work-001.jpg" alt="..." width={2861} height={3603} />
+          <Image src="/work-001.jpg" alt="..." width={2861} height={3603} />
+          <Image src="/work-001.jpg" alt="..." width={2861} height={3603} />
+          <Image src="/work-001.jpg" alt="..." width={2861} height={3603} />
+          <Image src="/work-001.jpg" alt="..." width={2861} height={3603} />
+          <Image src="/work-001.jpg" alt="..." width={2861} height={3603} />
+          <Image src="/work-001.jpg" alt="..." width={2861} height={3603} />
+        </div>
+      </section>
+    </div>
   )
 }
 
 function Header() {
   return (
-    <header className="relative h-screen w-screen">
+    <header className="relative h-screen w-full">
       <nav className="flex pt-16">
         <Image
           className="w-[45vw]"
@@ -36,7 +59,7 @@ function Header() {
       </nav>
 
       <Image
-        className="relative -z-10 h-screen w-screen object-cover"
+        className="relative -z-10 h-screen w-full object-cover"
         src="/home-background.png"
         alt="Tatuagem do personagem Luffy do anime One Piece"
         fill
@@ -53,12 +76,13 @@ type IndicationProps = {
 }
 
 function Indication({ posX, posY }: IndicationProps) {
-  const justifyContent = posX === 'left' ? 'justify-start' : 'justify-end'
   const alignItems = posY === 'lower' ? 'items-end' : 'items-start'
 
   return (
     <div
-      className={`flex w-screen flex-wrap ${alignItems} ${justifyContent} gap-4`}
+      className={cn(`flex w-full flex-wrap ${alignItems} justify-end gap-4`, {
+        'flex-row-reverse': posX === 'left',
+      })}
     >
       Especialista em tatuagens de anime e temática oriental
       <Image
